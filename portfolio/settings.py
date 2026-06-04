f07c9e6395c7ff9d094bbe6a8665b3542dd03808
unknown <vethsothi002@gmail.com>
2026-06-04 22:01:20 +0700

    add cloud file upload

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     # Third-party
     'rest_framework',
     'rest_framework_simplejwt',
@@ -113,6 +115,17 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Cloudinary — production media storage (active only when CLOUDINARY_CLOUD_NAME is set)
+_cloudinary_cloud = os.environ.get('CLOUDINARY_CLOUD_NAME', '')
+if _cloudinary_cloud:
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': _cloudinary_cloud,
+        'API_KEY': os.environ.get('CLOUDINARY_API_KEY', ''),
+        'API_SECRET': os.environ.get('CLOUDINARY_SECRET', ''),
+    }
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    MEDIA_URL = f'https://res.cloudinary.com/{_cloudinary_cloud}/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
